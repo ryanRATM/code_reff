a934871b8fb664c089755815ca929ad4cf17b9e6
Lucas Ryan <lucas.ryan@knights.ucf.edu>
2018-11-02 12:51:50 -0400

    more linked list

--- c/data_structures/linked_list/linked_list.c
+++ c/data_structures/linked_list/linked_list.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
+
 
 typedef struct l {
 	int data;
 	struct l* next;	
 } node;
 
+void printArray(int* arr, int len);
 void printList(node* list);
 int length(node* list);
 node* insertAtHead(node* list, int data);
 node* insertAtTail(node* list, int data);
 void removeNode(node* list, int data);
-
-// to be implemented
 int pop(node* list);
 int removeNodeAt(node* list, int index);
 bool contains(node* list, int data);
 bool equals(node* orig, node* test);
-node* merge(node* orig, node* extra);
+node* append(node* orig, node* extra);
+int frequency(node* list, int data);
+node** split(node* list, int numBins);
+node* reverse(node* list);
+int* toArray(node* list, int* len);
+node* filter(node* list, bool (*func)(int));
+node* map(node* list, int (*func)(int));
+
+
+//  helper functions for map and filter
+int mapPow(int x) { return pow(x, 2); }
+bool filterOdd(int x) { return x % 2 == 0; }
 
 
 int main() {
@@ -91,20 +103,87 @@ int main() {
 	printf("Does list contain %d: %c\n", 1, contains(list, 1) ? 'Y' : 'N');
 
 
-	node* otherList1 = NULL;
-	node* otherList2 = NULL;
+	node* otherList1 = NULL; // 200
+	node* otherList2 = NULL; // 2, 200
 	otherList1 = insertAtTail(otherList1, 200);
 	otherList2 = insertAtTail(otherList2, 200);
 	otherList2 = insertAtHead(otherList2, 2);
+
 
 	printf("Does list and otherList1 match: %c\n", (equals(list, otherList1)) ? 'Y' : 'N');
 	printf("Does list and otherList2 match: %c\n", (equals(list, otherList2)) ? 'Y' : 'N');
 	printf("Does otherList1 and otherList2 match: %c\n", (equals(otherList1, otherList2)) ? 'Y' : 'N');
 
+
+	append(otherList1, otherList2);
+	printf("length: %d\n", length(otherList1));
+	printList(otherList1); // 200, 2, 200
+
+
+
+	int otherList1Length = 0;
+	int* otherList1Data = toArray(otherList1, &otherList1Length);
+	printArray(otherList1Data, otherList1Length);
+
+
+
+	node* otherList2Rev = reverse(otherList2);
+	printf("length: %d\n", length(otherList2Rev));
+	printList(otherList2Rev); // 200, 2
+	
+
+	/* >ADD HERE< */
+	list = insertAtTail(list, 0);
+	list = insertAtTail(list, 1);
+	list = insertAtTail(list, 2);
+	list = insertAtTail(list, 3);
+	list = insertAtTail(list, 4);
+	list = insertAtTail(list, 5);
+	list = insertAtTail(list, 6);
+	list = insertAtTail(list, 7);
+	list = insertAtTail(list, 8);
+	list = insertAtTail(list, 9);
+	list = insertAtTail(list, 10);	
+	printf("length: %d\n", length(list));
+	printList(list);
+
+
+
+	node** bins = split(list, 3);
+	int i = 0;
+	for(i = 0; i < 3; i++) {
+		printf("bins[%d]: \n", i);
+		printList(bins[i]);
+	}
+
+
+	printf("list length: %d\n", length(list));
+	printList(list);
+
+
+	node* mapList = map(list, mapPow);
+	node* filtList = filter(list, filterOdd);
+
+	printf("list length: %d\n", length(list));
+	printList(list);
+	printf("mapList length: %d\n", length(mapList));
+	printList(mapList);
+	printf("filtList length: %d\n", length(filtList));
+	printList(filtList);
+
+
 	printf("All Done!\n");
 	return 0;
 }
 
+
+void printArray(int* arr, int len) {
+	printf("arr length: %d\n", len);
+	int i;
+	for(i = 0; i < len; i++) {
+		printf("arr[%d]: %d\n", i, arr[i]);
+	}
+}
 
 void printList(node* list) {
 	if(list == NULL) { return 0; }
@@ -138,7 +217,7 @@ node* insertAtHead(node* list, int data) {
 	head->next->data = data;
 	head->next->next = (list == NULL) ? list : list->next;
 	
-	//list = head;
+	list = head;
 
 	return head;
 }
@@ -232,6 +311,144 @@ bool equals(node* orig, node* test) {
 	return (origIter == NULL) && (testIter == NULL);
 }
 
-node* merge(node* orig, node* extra) {
-	return NULL;
+node* append(node* orig, node* extra) {
+	if(orig == NULL) { return extra; }
+	if(extra == NULL) { return orig; }
+
+	node* iter = orig;
+	while(iter->next != NULL) { iter = iter->next; }
+	
+	iter->next = extra->next;
+	
+	return orig;
+}
+
+
+int frequency(node* list, int data) {
+	if(list == NULL) { return 0; }
+
+	node* iter = list->next;
+	int cnt = 0;
+
+	while(iter != NULL) {
+		cnt += ((iter->data == data)? 0 : 1);
+		iter = iter->next;
+	}
+
+	return cnt;
+}
+
+node** split(node* list, int numBins) {
+	int len = length(list);
+	if(len == 0) { return NULL; }
+
+
+	node** bins = (node**) malloc(sizeof(node*) * numBins);
+	node* iter = list->next;
+	int perBin = (int) ceil(len / (double)(numBins));
+	int i, j;
+
+//	perBin += ((len % numBins) ? 0 : 1);
+	printf("len: %d; numBins: %d; perBin: %d\n", len, numBins, perBin);
+	
+	for(i = 0; i < numBins; i++) {
+		bins[i] = (node*) malloc(sizeof(node));
+		bins[i] = NULL;
+		for(j = 0; j < perBin && iter != NULL; j++) {
+			bins[i] = insertAtTail(bins[i], iter->data);
+			iter = iter->next;
+		}
+	}
+
+	return bins;
+}
+
+node* reverse(node* list) {
+	if(list == NULL) { return NULL; }
+
+	node* listRev = NULL;
+	node* iter = list->next;
+
+	while(iter != NULL) {
+		listRev = insertAtHead(listRev, iter->data);
+		iter = iter->next;
+	}
+	
+	return listRev;
+}
+
+int* toArray(node* list, int* len) {
+	(*len) = length(list);
+	if(len == 0) { return NULL; }
+
+	printf("[length: %d]\n", (*len));
+	int* values = (int*) malloc(sizeof(int) * (*len));
+	node* iter = list->next;
+	int i = 0;
+
+	while(iter != NULL) {
+		values[i++] = iter->data;
+		iter = iter->next;
+	}
+
+	return values;
+}
+
+
+node* map(node* list, int (*func)(int)) {
+	if(list == NULL) { return NULL; }
+	
+	node* iter = list->next;
+	node* mapList = (node*) malloc(sizeof(node));
+	mapList->next = (node*) malloc(sizeof(node));
+
+	node* mIter = mapList->next;
+
+
+	while(iter->next != NULL) {
+		mIter->data = func(iter->data);
+		mIter->next = (node*) malloc(sizeof(node));
+				
+		iter = iter->next;
+		mIter = mIter->next;
+	}
+
+	mIter->data = func(iter->data);
+	mIter->next = NULL;
+
+	return mapList;
+}
+
+node* filter(node* list, bool (*func)(int)) {
+	if(list == NULL) { return NULL; }
+
+	node* iter = list->next;
+	node* filterList = (node*) malloc(sizeof(node));
+	filterList->next = (node*) malloc(sizeof(node));
+
+	node* fIter = filterList->next;
+
+	if(iter->next == NULL) {
+		if(func(iter->data)) {
+			fIter->data = iter->data;
+			fIter->next = NULL;
+		}
+		return filterList;
+	}
+
+	while(iter->next->next != NULL) {
+		if(func(iter->data)) {
+			fIter->data = iter->data;
+			fIter-> next = (node*) malloc(sizeof(node));
+			fIter = fIter->next;
+		}
+		iter = iter->next;
+	}
+
+	if(func(iter->next->data)) {
+		fIter->data = iter->next->data;
+	}
+	fIter->next = NULL;
+	
+	return filterList;
 }
